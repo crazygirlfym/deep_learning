bd22215005faf77dcd1d5ace6564771a8ff22b0a
crazygirlfym <381847043@qq.com>
2018-05-27 15:29:45 +0800

    some tools for object detection

---
+++ object_detection/layer_utils.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+#-*- coding: utf-8 -*-
+
+# File Name: layer_utils.py
+# Author: Yanmei Fu
+# mail: yanmei2016@iscas.ac.cn
+# Created Time: 2018-05-27
+import tensorflow as tf
+
+def __variable_with_weight_decay(kernel_shape, initializer, wd):
+    """
+    Create a variable with L2 Regularization (Weight Decay)
+    :param kernel_shape: the size of the convolving weight kernel.
+    :param initializer: The initialization scheme, He et al. normal or Xavier normal are recommended.
+    :param wd:(weight decay) L2 regularization parameter.
+    :return: The weights of the kernel initialized. The L2 loss is added to the loss collection.
+    """
+    w = tf.get_variable('weights', kernel_shape, tf.float32, initializer=initializer)
+
+    collection_name = tf.GraphKeys.REGULARIZATION_LOSSES
+    if wd and (not tf.get_variable_scope().reuse):
+        weight_decay = tf.multiply(tf.nn.l2_loss(w), wd, name='w_loss')
+        tf.add_to_collection(collection_name, weight_decay)
+    return w
+
+# Summaries for variables
+def __variable_summaries(var):
+    """
+    Attach a lot of summaries to a Tensor (for TensorBoard visualization).
+    :param var: variable to be summarized
+    :return: None
+    """
+    with tf.name_scope('summaries'):
+        mean = tf.reduce_mean(var)
+        tf.summary.scalar('mean', mean)
+        with tf.name_scope('stddev'):
+            stddev = tf.sqrt(tf.reduce_mean(tf.square(var - mean)))
+        tf.summary.scalar('stddev', stddev)
+        tf.summary.scalar('max', tf.reduce_max(var))
+        tf.summary.scalar('min', tf.reduce_min(var))
+        tf.summary.histogram('histogram', var)
+
+
